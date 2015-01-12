421f01b4ce41a3a6c8fe5bf04a328c6aef7f5ae9
Khaled Garbaya <khaledgarbaya@gmail.com>
2015-01-12 20:36:59 +0100

    Initial Commit

---
+++ SDL_Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SDL_Game
+//
+//  Created by Khaled Garbaya on 12/01/15.
+//  Copyright (c) 2015 Khaled Garbaya. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
